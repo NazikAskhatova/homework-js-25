f643fef43e8bf4c6aaa7dffae3254abdce232c85
wiusxt <ashatovanas65@gmail.com>
2022-02-25 15:50:28 +0600

    added new task

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ details.forEach((detail) => {
         article.style.display = 'none';
     })
 })
+
+const main = document.querySelector('main');
+articles.forEach(article => {
+  const articleElement = document.createElement('article');
+  const h1 = document.createElement('h1');
+  const img = document.createElement('img');
+  const p = document.createElement('p');
+  const a = document.createElement('a');
+
+  articleElement.id = article.id;
+  h1.textContent = article.title;
+  img.src = article.image;
+  p.textContent = article.body;
+  a.href = './article.html';
+  a.style.textDecoration = "none";
+  a.style.color = 'black';
+
+  articleElement.append(a);
+  a.appendChild(h1);
+  articleElement.append(img);
+  articleElement.append(p);
+  main.append(articleElement);
+})
