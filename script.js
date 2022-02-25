390b436465e5e081c1b0fe78aa5cfd04ccd75380
wiusxt <ashatovanas65@gmail.com>
2022-02-25 15:57:43 +0600

    added new task with url

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ details.forEach((detail) => {
     })
 })
 
+const articles = [
+    {
+      id: "article-1",
+      title: "Hello world",
+      image: "./koala.jpg",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+    },
+    {
+      id: "article-2",
+      title: "Hello world",
+      image: "./koala.jpg",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+    },
+    {
+      id: "article-3",
+      title: "Hello world",
+      image: "./koala.jpg",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+    }
+  ];
+  
+
 const main = document.querySelector('main');
 articles.forEach(article => {
   const articleElement = document.createElement('article');
